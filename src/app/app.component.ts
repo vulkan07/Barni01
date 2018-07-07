736c0ddd128e05e3a8e941528cd87c661efc69a5
Vulkan07 <olahbozobarnabas@gmail.com>
2018-07-07 02:18:27 +0200

    hozzáadva 2 szinező gomb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  className = 'donaldiusz';
   bugyi = 'fos';
-  activate() {
-    console.log('sikeresen megnyomva');
-    alert('Kattints az "OK"-ra a youtube megnyitásához.');
-    window.open('https://www.youtube.com' );
 
+  activate() {
+    console.log('youtube gomb megnyomva');
+    alert('Kattints az "OK"-ra a youtube megnyitásához.');
+    window.open('https://www.youtube.com/channel/UCc-NRCHc6kt5PeoBGgF_lJg' );
+  }
+
+  color() {
+    console.log('pink gomb sikeresen megnyomva');
+    this.className = 'donaldiusz2';
+  }
+  color2() {
+    console.log('zöld gomb sikeresen megnyomva');
+    this.className = 'donaldiusz';
   }
 }
